441ddc20a5ec3de4b543d44196b3e083abaa1f96
Antony Gladson <antonygr@juniper.net>
2025-07-01 15:54:25 +0530

    increase poll interval

--- telegram_bot.py
+++ telegram_bot.py
@@ -548,7 +548,7 @@ def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_command))
-    app.run_polling()
+    app.run_polling(poll_interval=10, timeout=10, drop_pending_updates=False, allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
